4808db65530aaf00af39ee7ee69640855a91f1bd
JulieLenoir <renoujulie1@gmail.com>
2025-10-10 12:11:15 +0200

    wip update v1

--- src/Controller/Api/AppointmentController.php
+++ src/Controller/Api/AppointmentController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api/appointments', name: 'api_appointments_')]
+#[Route('/api/v1/appointments', name: 'api_appointments_')]
 class AppointmentController extends AbstractController
 {
     // GET /api/appointments → liste tous les rendez-vous
